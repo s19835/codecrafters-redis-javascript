fa3196d8522327ca049502415eb9e262edca5fc6
s19835 <s19835@sci.pdn.ac.lk>
2024-07-24 00:20:40 +0530

    Implement the ECHO command #QQ0

--- app/main.js
+++ app/main.js
@@ -1,10 +1,33 @@
 import net from "net";
 
+// Implement a Redis protocol parser
+function redisProtocolParser(data) {
+    return `$${data.length}\r\n${data}\r\n`;
+}
 
 const server = net.createServer((connection) => {
   // Handle connection
-  connection.on("data", () => {
-    connection.write('+PONG\r\n');
+  connection.on("data", (data) => {
+    const recived = data.toString().split('\r\n');
+    
+    if (!recived) {
+        throw new Error("Invalid input");
+    }
+
+    const command = recived[2].toUpperCase();
+
+    switch (command) {
+        case 'PING':
+            connection.write('+PONG\r\n');
+            break;
+        
+        case 'ECHO':
+            connection.write(redisProtocolParser(recived[4]));
+            break;
+
+        default:
+            break;
+    }
   })
 });
 
