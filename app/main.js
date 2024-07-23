a5dad82955c8526e806c17798d02f51b158e3a32
s19835 <s19835@sci.pdn.ac.lk>
2024-07-23 08:14:11 +0530

    ES6 mdoule

--- app/main.js
+++ app/main.js
@@ -1,4 +1,4 @@
-const net = require("net");
+import net from "net";
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
