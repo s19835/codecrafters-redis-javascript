521efad686264369d5303f1e817adb5b06b4bbe0
s19835 <s19835@sci.pdn.ac.lk>
2024-07-23 23:37:21 +0530

    Respond to multiple PINGs #WY1

--- app/main.js
+++ app/main.js
@@ -3,7 +3,9 @@ import net from "net";
 
 const server = net.createServer((connection) => {
   // Handle connection
-  connection.write('+PONG\r\n');
+  connection.on("data", () => {
+    connection.write('+PONG\r\n');
+  })
 });
 
 server.listen(6379, "127.0.0.1");
