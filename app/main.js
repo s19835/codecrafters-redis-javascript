f10bf955c95e8ab0b16c5156a93e7f85aeb3d001
s19835 <s19835@sci.pdn.ac.lk>
2024-07-25 21:45:42 +0530

    Configure listening port #BW1

--- app/main.js
+++ app/main.js
@@ -1,6 +1,16 @@
 import net from "net";
 
-const store = {} // Object for stroing key value pair for GET, SET
+// Object for stroing key value pair for GET, SET
+const store = {};
+
+// Get the specified port from input
+const args = process.argv;
+let port;
+if (args.includes('--port')) { 
+    port = parseInt(args[args.indexOf('--port') + 1]); 
+} else {
+    port = 6379;
+}
 
 // Implement a Redis protocol parser
 function redisProtocolParser(data) {
@@ -54,4 +64,4 @@ const server = net.createServer((connection) => {
   })
 });
 
-server.listen(6379, "127.0.0.1");
+server.listen(port, "127.0.0.1");
